18afdfc1cc30ec5de3cef6ebc855b0b267fb9c80
hemit patel 🧑🏽‍💻 <hemitvpatel@gmail.com>
2026-02-12 21:25:02 +0000

    update stuff

---
+++ ai-agents/Memory_agent.py
@@ -0,0 +1,38 @@
+import os 
+from typing import TypedDict, List, Union 
+from langchain_core.messages import HumanMessage, AIMessage
+from langchain_openai import ChatOpenAI
+from langgraph.graph import StateGraph, START, END
+from dotenv import load_dotenv
+
+load_dotenv()
+
+class AgentState(TypedDict):
+    messages: List[Union[HumanMessage, AIMessage]]
+    
+llm = ChatOpenAI(model="gpt-4o")
+
+def process(state: AgentState) -> AgentState:
+    """This node will solve the request you input"""
+    response = llm.invoke(state['messages'])
+
+    state['messages'].append(AIMessage(content=response.content))
+    print(f"\nAI: {response.content}")
+
+
+graph = StateGraph(AgentState)
+graph.add_node("process", process)
+
+graph.add_edge(START, "process")
+graph.add_edge("process", END)
+agent = graph.compile()
+
+user_input = input("Enter: ")
+
+conversation_history = []
+
+while user_input != "exit": 
+    conversation_history.append(HumanMessage(content=user_input))
+    result = agent.invoke({"messages": conversation_history})
+    conversation_history = result['messages']
+    user_input = input("Enter: ")
